0da637d29292da3ad0fed01d6ffe1ecfa4e4a01a
Santiago Carreño <scarrenov@gmail.com>
2021-02-25 10:56:35 -0500

    Created project

---
+++ Caso Aerotaka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Caso_Aerotaka
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
