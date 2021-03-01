4e232f147531a0378f25491911acc9921b236d28
AndrressBrrrrGod <ludwingandres807@gmail.com>
2021-02-28 20:10:43 -0500

    creation of the interactive menu
    ....

--- Caso Aerotaka/Program.cs
+++ Caso Aerotaka/Program.cs
@@ -6,6 +6,48 @@ namespace Caso_Aerotaka
     {
         private static void Main()
         {
+            ObjectController.Cargar(out var clientes,out var reservas,out var distribuciones,out var personal,out var aeronaves,out var trayectos);
+            Console.WriteLine("Ingresar como:");
+            Console.WriteLine("1.Cliente");
+            Console.WriteLine("2.Departamento de mercadeo");
+            Console.WriteLine("3.Administracion");
+            var index = Convert.ToInt32(Console.ReadLine());
+            switch (index)
+            {
+                case 1:
+                    Console.WriteLine("Siga las siguientes instrucciones:");
+                    Console.WriteLine("1.Registrarse");
+                    Console.WriteLine("2.Reservar vuelo");
+                    
+                    var usuarioIndex = Convert.ToInt32(Console.ReadLine());
+
+                    switch (usuarioIndex)
+                    {
+                        case 1:
+                            Console.WriteLine("Ingrese Id");
+                            var idUsuario = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Ingrese nombre completo");
+                            var nombreCompleto = Console.ReadLine();
+                            Console.WriteLine("Ingrese el correo");
+                            var correoUsuario = Console.ReadLine();
+                            Console.WriteLine("Digite el telefono");
+                            var telefonoUsuario = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Digite la ciudad");
+                            var ciudad = Console.ReadLine();
+
+                            var nuevoCliente = new Cliente(idUsuario, nombreCompleto, correoUsuario, telefonoUsuario,
+                                ciudad);
+                            clientes.Add(nuevoCliente);
+                            ObjectController.GuardarClientes(clientes);
+                            
+                            case 2:
+                                if()
+                            
+                            break;
+                    }
+                    
+                    break;
+            }
             
         }
 
